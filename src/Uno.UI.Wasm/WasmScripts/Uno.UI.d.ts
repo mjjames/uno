06e5c1983a6e559319ad6d846917983e69a434a2
Dr.Rx <david.rey@nventive.com>
2019-11-18 15:50:21 -0500

    Add support of the "SV.ChangeView" on wasm

--- src/Uno.UI.Wasm/WasmScripts/Uno.UI.d.ts
+++ src/Uno.UI.Wasm/WasmScripts/Uno.UI.d.ts
@@ -449,6 +449,7 @@ declare namespace Uno.UI {
         private static MAX_WIDTH;
         private static MAX_HEIGHT;
         private measureViewInternal;
+        scrollTo(pParams: number): boolean;
         setImageRawData(viewId: number, dataPtr: number, width: number, height: number): string;
         /**
          * Sets the provided image with a mono-chrome version of the provided url.
@@ -607,6 +608,15 @@ declare class WindowManagerResetStyleParams {
     Styles_Length: number;
     Styles: Array<string>;
     static unmarshal(pData: number): WindowManagerResetStyleParams;
+}
+declare class WindowManagerScrollToOptionsParams {
+    Left: number;
+    Top: number;
+    HasLeft: boolean;
+    HasTop: boolean;
+    DisableAnimation: boolean;
+    HtmlId: number;
+    static unmarshal(pData: number): WindowManagerScrollToOptionsParams;
 }
 declare class WindowManagerSetAttributeParams {
     HtmlId: number;
